9b36e0efb5b931c414abae561cc02287c8dc91bc
Diana <42554824+JUPiTERdia117@users.noreply.github.com>
2025-03-16 23:21:26 -0600

    Prac6

--- ConfigInicial/Carga de Modelos.cpp
+++ ConfigInicial/Carga de Modelos.cpp
@@ -1,6 +1,6 @@
 
 //Practica#6 (Carga de modelos) Monroy Salazar
-//Fecha de entrega 11 de Marzo de 2025
+//Fecha de entrega 16 de Marzo de 2025
 //315118894
 
 
@@ -61,7 +61,7 @@ int main( )
     glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Carga de modelos y camara sintetica Monroy Salazar", nullptr, nullptr );
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Practica 6 Monroy Salazar", nullptr, nullptr );
     
     if ( nullptr == window )
     {
@@ -102,7 +102,15 @@ int main( )
     
     // Load models
     Model dog((char*)"Models/RedDog.obj");
+    Model chair((char*)"Models/Chair/kursi.obj");
+    Model tablec((char*)"Models/Table/Chair_and_Table_School.obj");
+    Model room((char*)"Models/Room/Room.obj");
+    Model board((char*)"Models/Piza/pizar.obj");
+    /*Model classroom((char*)"Models/Classroom/Classroom.obj");*/
+
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
+
+
     
   
 
@@ -128,18 +136,109 @@ int main( )
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-        // Draw the loaded model
-        glm::mat4 model(1);
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+
+
+        //Cuarto
+        glm::mat4 modelR(1);
+        modelR = glm::translate(modelR, glm::vec3(0.0f, -0.45f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelR));
+        room.Draw(shader);
+
+        //Silla
+        glm::mat4 modelChair(1);
+        modelChair = glm::translate(modelChair, glm::vec3(0.0f, -0.5f, 0.0f));
+        modelChair = glm::scale(modelChair, glm::vec3(0.4f, 0.4f, 0.4f));
+        modelChair = glm::rotate(modelChair, -55.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelChair));
+        chair.Draw(shader);
+
+        //Silla2
+        glm::mat4 modelChair2(1);
+        modelChair2 = glm::translate(modelChair2, glm::vec3(-1.0f, -0.5f, 0.0f));
+        modelChair2 = glm::scale(modelChair2, glm::vec3(0.4f, 0.4f, 0.4f));
+        modelChair2 = glm::rotate(modelChair2, -55.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelChair2));
+        chair.Draw(shader);
+
+        //Silla3
+        glm::mat4 modelChair3(1);
+        modelChair3 = glm::translate(modelChair3, glm::vec3(1.0f, -0.5f, 0.0f));
+        modelChair3 = glm::scale(modelChair3, glm::vec3(0.4f, 0.4f, 0.4f));
+        modelChair3 = glm::rotate(modelChair3, -55.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelChair3));
+        chair.Draw(shader);
+
+        //Silla4
+        glm::mat4 modelChair4(1);
+        modelChair4 = glm::translate(modelChair4, glm::vec3(0.0f, -0.5f, 1.0f));
+        modelChair4 = glm::scale(modelChair4, glm::vec3(0.4f, 0.4f, 0.4f));
+        modelChair4 = glm::rotate(modelChair4, -55.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelChair4));
+        chair.Draw(shader);
+
+        //Silla5
+        glm::mat4 modelChair5(1);
+        modelChair5 = glm::translate(modelChair5, glm::vec3(-1.0f, -0.5f, 1.0f));
+        modelChair5 = glm::scale(modelChair5, glm::vec3(0.4f, 0.4f, 0.4f));
+        modelChair5 = glm::rotate(modelChair5, -55.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelChair5));
+        chair.Draw(shader);
+
+        //Silla3
+        glm::mat4 modelChair6(1);
+        modelChair6 = glm::translate(modelChair6, glm::vec3(1.0f, -0.5f, 1.0f));
+        modelChair6 = glm::scale(modelChair6, glm::vec3(0.4f, 0.4f, 0.4f));
+        modelChair6 = glm::rotate(modelChair6, -55.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelChair6));
+        chair.Draw(shader);
+
+
+        // Draw the loaded model dogo
+        glm::mat4 mDog(1);
+        mDog = glm::translate(mDog, glm::vec3(-2.2f, 0.58f, -1.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(mDog));
         dog.Draw(shader);
 
 
+        //Pizarron
+        glm::mat4 modelBoard(1);
+        modelBoard = glm::translate(modelBoard, glm::vec3(0.0f, 0.0f, -3.0f));
+        modelBoard = glm::scale(modelBoard, glm::vec3(0.01f, 0.01f, 0.01f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelBoard));
+        board.Draw(shader);
 
-        model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
+
+        //Mesa
+        glm::mat4 modelTable(1);
+        modelTable = glm::translate(modelTable, glm::vec3(-2.3f, -0.5f, -2.0f));
+        modelTable = glm::rotate(modelTable, 135.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        modelTable = glm::scale(modelTable, glm::vec3(0.7f, 0.7f, 0.7f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelTable));
+        tablec.Draw(shader);
+
+
+
+
+
+
+
+        
+        
+
+       
+        
+
+        /*glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        classroom.Draw(shader);*/
+
+
+
+       /* model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
 
         model = glm::rotate(model, 135.0f, glm::vec3(0.0f, 0.0f, 1.0f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        dog.Draw(shader);
+        dog.Draw(shader);*/
 
 
 
